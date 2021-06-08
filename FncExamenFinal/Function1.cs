d480f05c39b03d4263faf20684350926a2f54475
Alejandra Salazar Rojas <a2017113913@estudiantes.upsa.edu.bo>
2021-06-07 20:46:19 -0400

    push

--- FncExamenFinal/Function1.cs
+++ FncExamenFinal/Function1.cs
@@ -1,16 +1,32 @@
 using System;
+using System.Threading.Tasks;
+using FncExamenFinal.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace FncExamenFinal
 {
     public static class Function1
     {
+    
         [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("colaexamenfinal", Connection = "MyConn")]string myQueueItem, ILogger log)
+        public static async Task RunAsync(
+            [ServiceBusTrigger("colaexamenfinal", Connection = "MyConn")] string myQueueItem,
+            [CosmosDB(databaseName: "dbartificial", collectionName: "eventos", ConnectionStringSetting = "strCosmos")] IAsyncCollector<object> datos,
+            ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            try
+            {
+                log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+                var data = JsonConvert.DeserializeObject<Data>(myQueueItem);
+                await datos.AddAsync(data);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"No fue posible insertar datos: {e.Message}");
+            }
         }
     }
 }
---
+++ FncExamenFinal/Models/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FncExamenFinal.Models
+{
+    class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+        [Required]
+        public string Event { get; set; }
+    }
+}
